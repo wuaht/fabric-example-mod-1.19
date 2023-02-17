8b5e171039c6497a6f661ec489e32046422681b0
wuaht <wuahtm8@gmail.com>
2023-02-17 14:54:06 +0100

    Initial commit

---
+++ src/main/java/net/wuaht/testmod/TestMod.java
@@ -0,0 +1,15 @@
+package net.wuaht.testmod;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestMod implements ModInitializer {
+	public static final String MOD_ID = "testmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/wuaht/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.wuaht.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
