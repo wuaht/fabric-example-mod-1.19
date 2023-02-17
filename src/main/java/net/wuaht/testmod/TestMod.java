cc9d3c3e80ab909fbd2f85d2fc4fc0945cde0854
wuaht <wuahtm8@gmail.com>
2023-02-17 14:55:01 +0100

    important comment

--- src/main/java/net/wuaht/testmod/TestMod.java
+++ src/main/java/net/wuaht/testmod/TestMod.java
@@ -4,6 +4,7 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+// comment
 public class TestMod implements ModInitializer {
 	public static final String MOD_ID = "testmod";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
