f5299f2f2d99b17779245cee29f2122ce3267825
wuaht <wuahtm8@gmail.com>
2023-02-17 21:35:12 +0100

    Added Tooltips

--- src/main/java/net/wuaht/testmod/item/custom/EightBallItem.java
+++ src/main/java/net/wuaht/testmod/item/custom/EightBallItem.java
@@ -1,15 +1,32 @@
 package net.wuaht.testmod.item.custom;
 
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class EightBallItem extends Item {
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Text.literal("Right-Click to get a random number!").formatted(Formatting.AQUA));
+        } else {
+            tooltip.add(Text.literal("Press Shift for more info!").formatted(Formatting.YELLOW));
+        }
+
+        super.appendTooltip(stack, world, tooltip, context);
+    }
 
     @Override
     public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
