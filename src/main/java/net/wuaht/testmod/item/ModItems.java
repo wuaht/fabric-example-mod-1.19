58e79e77c7c8cdb75191912c3bd0a7a22f28f86b
wuaht <wuahtm8@gmail.com>
2023-02-17 20:34:31 +0100

    Added tanzanite functionality

--- src/main/java/net/wuaht/testmod/item/ModItems.java
+++ src/main/java/net/wuaht/testmod/item/ModItems.java
@@ -3,7 +3,6 @@ package net.wuaht.testmod.item;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemGroups;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -18,6 +17,7 @@ public class ModItems {
     public static final Item TANZANITE = registerItem("tanzanite",
             new Item(new Item.Settings()),
             ModItemGroup.TANZANITE);
+
 
     private static Item registerItem(String name, Item item, ItemGroup group) {
         ItemGroupEvents.modifyEntriesEvent(group).register(entries -> entries.add(item));
